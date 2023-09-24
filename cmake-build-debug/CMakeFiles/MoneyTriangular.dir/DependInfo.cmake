67af0b6e1d25d2da5e21057e7cda4a6159051229
M3GUMI <1037972328@qq.com>
2023-09-24 19:47:08 +0800

    try

---
+++ cmake-build-debug/CMakeFiles/MoneyTriangular.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/yyg/conf/conf.cpp" "CMakeFiles/MoneyTriangular.dir/conf/conf.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/conf/conf.cpp.o.d"
+  "/home/ubuntu/yyg/conf/strategy.cpp" "CMakeFiles/MoneyTriangular.dir/conf/strategy.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/conf/strategy.cpp.o.d"
+  "/home/ubuntu/yyg/define/define.cpp" "CMakeFiles/MoneyTriangular.dir/define/define.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/define/define.cpp.o.d"
+  "/home/ubuntu/yyg/define/error.cpp" "CMakeFiles/MoneyTriangular.dir/define/error.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/define/error.cpp.o.d"
+  "/home/ubuntu/yyg/lib/api/http/base_api_wrapper.cpp" "CMakeFiles/MoneyTriangular.dir/lib/api/http/base_api_wrapper.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/api/http/base_api_wrapper.cpp.o.d"
+  "/home/ubuntu/yyg/lib/api/http/binance_api_wrapper.cpp" "CMakeFiles/MoneyTriangular.dir/lib/api/http/binance_api_wrapper.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/api/http/binance_api_wrapper.cpp.o.d"
+  "/home/ubuntu/yyg/lib/api/ws/binance_depth_wrapper.cpp" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/binance_depth_wrapper.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/binance_depth_wrapper.cpp.o.d"
+  "/home/ubuntu/yyg/lib/api/ws/binance_order_wrapper.cpp" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/binance_order_wrapper.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/binance_order_wrapper.cpp.o.d"
+  "/home/ubuntu/yyg/lib/api/ws/websocket_wrapper.cpp" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/websocket_wrapper.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/api/ws/websocket_wrapper.cpp.o.d"
+  "/home/ubuntu/yyg/lib/arbitrage/triangular/ioc_triangular.cpp" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/ioc_triangular.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/ioc_triangular.cpp.o.d"
+  "/home/ubuntu/yyg/lib/arbitrage/triangular/maker_triangular.cpp" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/maker_triangular.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/maker_triangular.cpp.o.d"
+  "/home/ubuntu/yyg/lib/arbitrage/triangular/triangular.cpp" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/triangular.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/arbitrage/triangular/triangular.cpp.o.d"
+  "/home/ubuntu/yyg/lib/capital_pool/capital_pool.cpp" "CMakeFiles/MoneyTriangular.dir/lib/capital_pool/capital_pool.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/capital_pool/capital_pool.cpp.o.d"
+  "/home/ubuntu/yyg/lib/executor/executor.cpp" "CMakeFiles/MoneyTriangular.dir/lib/executor/executor.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/executor/executor.cpp.o.d"
+  "/home/ubuntu/yyg/lib/order/order.cpp" "CMakeFiles/MoneyTriangular.dir/lib/order/order.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/order/order.cpp.o.d"
+  "/home/ubuntu/yyg/lib/pathfinder/graph.cpp" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/graph.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/graph.cpp.o.d"
+  "/home/ubuntu/yyg/lib/pathfinder/node.cpp" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/node.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/node.cpp.o.d"
+  "/home/ubuntu/yyg/lib/pathfinder/pathfinder.cpp" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/pathfinder.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/lib/pathfinder/pathfinder.cpp.o.d"
+  "/home/ubuntu/yyg/main.cpp" "CMakeFiles/MoneyTriangular.dir/main.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/main.cpp.o.d"
+  "/home/ubuntu/yyg/utils/utils.cpp" "CMakeFiles/MoneyTriangular.dir/utils/utils.cpp.o" "gcc" "CMakeFiles/MoneyTriangular.dir/utils/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MoneyTriangular.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MoneyTriangular.
+# This may be replaced when dependencies are built.
